499bda0cf982714c90d48f212dc0fcc43d5183e8
fujuntao <18258134916@163.com>
2023-06-08 21:55:15 +0800

    适配不同屏幕大小

--- lib/pages/hotel_screen.dart
+++ lib/pages/hotel_screen.dart
@@ -13,9 +13,9 @@ class HotelScreen extends StatelessWidget {
     final size = AppLayout.getSize(context);
     return Container(
       width: size.width*0.6,
-      height: 350,
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
-      margin: const EdgeInsets.only(right: 17, top: 5,),
+      height: AppLayout.getHeight(350),
+      padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15), vertical: AppLayout.getHeight(17)),
+      margin: EdgeInsets.only(right: AppLayout.getHeight(17), top: AppLayout.getHeight(5),),
       decoration: BoxDecoration(
         color: Styles.primaryColor,
         borderRadius: BorderRadius.circular(24),
@@ -31,7 +31,7 @@ class HotelScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 180,
+            height: AppLayout.getHeight(180),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
               color: Styles.primaryColor,
@@ -41,11 +41,11 @@ class HotelScreen extends StatelessWidget {
               )
             ),
           ),
-          const Gap(10),
+          Gap(AppLayout.getHeight(10)),
           Text("${hotel['place']}", style: Styles.headLineStyle2.copyWith(color: Styles.kakiColor)),
-          const Gap(5),
+          Gap(AppLayout.getHeight(5)),
           Text("${hotel['destination']}", style: Styles.headLineStyle3.copyWith(color: Colors.white,)),
-          const Gap(8),
+          Gap(AppLayout.getHeight(8)),
           Text("\$${hotel['price']}/night", style: Styles.headLineStyle1.copyWith(color: Styles.kakiColor,)),
         ],
       ),
