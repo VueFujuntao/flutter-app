08ad86ac3167c808d534c2c8651f9f3c371d27cf
fujuntao <18258134916@163.com>
2023-06-10 07:58:42 +0800

    搜索页面

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,13 +1,68 @@
 import 'package:app/utils/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import '../utils/app_layout.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+
     return Scaffold(
       backgroundColor: Styles.bgColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20), vertical: AppLayout.getHeight(20)),
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          Text("What are\n you looking for?", style: Styles.headLineStyle1.copyWith(fontSize: AppLayout.getWidth(35))),
+          Gap(AppLayout.getHeight(20)),
+          FittedBox(
+            child: Container(
+              padding: const EdgeInsets.all(3.5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
+                color: const Color(0xFFF4F6FD),
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    width: size.width*0.44,
+                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.horizontal(left: Radius.circular(AppLayout.getHeight(50))),
+                      color: Colors.white,
+                    ),
+                    child: const Center(
+                      child: Text("Airline ticket"),
+                    ),
+                  ),
+                  Container(
+                    width: size.width*0.44,
+                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.horizontal(right: Radius.circular(AppLayout.getHeight(50))),
+                      color: Colors.transparent,
+                    ),
+                    child: const Center(
+                      child: Text("Airline ticket"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            child: Row(
+              children: [
+                const Icon(Icons.flight_takeoff_rounded, color: Color(0xFFBFC2DF),),
+                Text("Departure")
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
